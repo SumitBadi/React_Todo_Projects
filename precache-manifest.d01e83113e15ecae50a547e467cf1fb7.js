01ced4148c46adc5cfde42bfbf6d27bf02706b7c
SumitBadi <100030935+SumitBadi@users.noreply.github.com>
2022-04-08 13:55:21 +0530

    Updates

---
+++ precache-manifest.d01e83113e15ecae50a547e467cf1fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fe006f01b0ebe7d3484610009102be",
+    "url": "/React_Todo_Projects/index/index.html"
+  },
+  {
+    "revision": "582ac6c8129b73caf18f",
+    "url": "/React_Todo_Projects/index/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "9018cdeaf0c50f3ba288",
+    "url": "/React_Todo_Projects/index/static/css/main.8c43ba23.chunk.css"
+  },
+  {
+    "revision": "582ac6c8129b73caf18f",
+    "url": "/React_Todo_Projects/index/static/js/2.3377c9d2.chunk.js"
+  },
+  {
+    "revision": "9018cdeaf0c50f3ba288",
+    "url": "/React_Todo_Projects/index/static/js/main.667c5085.chunk.js"
+  },
+  {
+    "revision": "56b7afa6471a1e83a616",
+    "url": "/React_Todo_Projects/index/static/js/runtime~main.87110144.js"
+  }
+]);
